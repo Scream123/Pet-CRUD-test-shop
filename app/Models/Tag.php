bd4c9313cfd92cfb45e8b4be3bf069d1682f94b9
Volodymyr Marakhovskyi <fly_life@mail.ru>
2024-08-09 22:17:30 +0300

    edited Tag Model

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,34 +2,19 @@
 
 namespace App\Models;
 
-use App\Helpers\SlugHelper;
+use App\Traits\Slugable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
 
 class Tag extends Model
 {
     use HasFactory;
+    use Slugable;
 
     protected $fillable = ['name', 'slug'];
 
-    public static function boot()
-    {
-        parent::boot();
-
-        static::creating(function ($model) {
-            $model->slug = SlugHelper::generateSlug(self::class, $model->name);
-        });
-
-        static::updating(function ($model) {
-            if ($model->isDirty('name')) {
-                $model->slug = SlugHelper::generateSlug(self::class, $model->name);
-            }
-        });
-    }
-
     public function products()
     {
-        return $this->belongsToMany(Product::class, 'product_category_tags', 'tag_id', 'product_id');
+        return $this->belongsToMany(Product::class, 'product_tags');
     }
 }
