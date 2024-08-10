e52117be784fee6647ca60942d10c25d2b041cea
Volodymyr Marakhovskyi <fly_life@mail.ru>
2024-08-10 06:08:59 +0300

    added constants from class Schema

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Schema\CategorySchema;
+use App\Schema\ProductCategorySchema;
 use App\Traits\Slugable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -11,21 +13,25 @@ class Category extends Model
     use HasFactory;
     use Slugable;
 
-    protected $fillable = ['name', 'slug', 'parent_id'];
+    protected $fillable = [
+        CategorySchema::NAME,
+        CategorySchema::SLUG,
+        CategorySchema::PARENT_ID,
+    ];
 
     public function parent()
     {
-        return $this->belongsTo(Category::class, 'parent_id');
+        return $this->belongsTo(Category::class, CategorySchema::PARENT_ID);
     }
 
     // Получение дочерних категорий
     public function children()
     {
-        return $this->hasMany(Category::class, 'parent_id');
+        return $this->hasMany(Category::class, CategorySchema::PARENT_ID);
     }
 
     public function products()
     {
-        return $this->belongsToMany(Product::class, 'product_categories', 'category_id', 'product_id');
+        return $this->belongsToMany(Product::class, ProductCategorySchema::TABLE, ProductCategorySchema::CATEGORY_ID, ProductCategorySchema::PRODUCT_ID);
     }
 }
