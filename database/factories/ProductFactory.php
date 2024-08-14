bad84750e8a408f9b23b837d98df2dfac7514933
Volodymyr Marakhovskyi <fly_life@mail.ru>
2024-08-14 23:02:18 +0300

    edited ProductFactory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,9 +2,7 @@
 
 namespace Database\Factories;
 
-use App\Models\Category;
 use App\Models\Product;
-use App\Models\Tag;
 use App\Schema\ProductSchema;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -12,23 +10,12 @@ class ProductFactory extends Factory
 {
     protected $model = Product::class;
 
-    public function definition()
+    public function definition(): array
     {
         return [
             ProductSchema::NAME => $this->faker->unique()->word,
             ProductSchema::SLUG => $this->faker->unique()->slug,
             ProductSchema::DESCRIPTION => $this->faker->paragraph,
         ];
-    }
-
-    public function withCategoryAndTags()
-    {
-        return $this->afterCreating(function (Product $product) {
-            $category = Category::factory()->create();
-            $tags = Tag::factory()->count(2)->create();
-
-            $product->categories()->attach($category->id);
-            $product->tags()->sync($tags->pluck('id')->toArray());
-        });
     }
 }
